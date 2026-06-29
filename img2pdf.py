98892fdc68d4923d1d62ab5d47fa36096ebdfba3
Damon Murdoch <damonmurdoch1@gmail.com>
2026-06-30 01:46:11 +1000

    Several improvements to img2pdf.py
    Several improvements to img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -1,111 +1,160 @@
-# Operating System libraries
-import sys, os
+import os, time
+
+import argparse
+import tempfile
+
+# Zip File Library
+import zipfile
 
 # Python PDF library
 import PyPDF2
 
-# For waiting for the file to create
-import time
-
-# Python progress bar library
-from tqdm import tqdm
-
 # Python image library
 from PIL import Image
 
-# make_pdf(path: String, writer: PdfFileWriter): Void
-# Given a folder path string and an (optional) PdfFileWriter
+parser = argparse.ArgumentParser(description="Image to Pdf Converter")
+parser.add_argument(
+    "path",
+    type=str,
+    nargs="+",
+    help="Path (or list of paths) to convert into pdf files",
+)
+parser.add_argument(
+    "-a",
+    "--archive",
+    action="store_true",
+    required=False,
+    help="If this switch is set, archive files (e.g. cbz) will be extracted and processed",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str, 
+    required=False,
+    help="temp",
+)
+
+
+# make_pdf(path: String, writer: PdfWriter): Void
+# Given a folder path string and an (optional) PdfWriter
 # object, builds a pdf using all of the image files in the folder
 # (Sorted by file name) and writes it.
-def make_pdf(path,writer=None):
+def make_pdf(path, output, writer=None):
 
-    # List of images in the folder
-    images = []
+    filename = f"{os.path.basename(path)}.pdf"
 
-    # If no writer object is provided
-    if(not writer):
-        # Create a new PdfFileWriter object
-        writer = PyPDF2.PdfFileWriter()
+    # Generate the output filepath
+    outpath = os.path.join(output, filename)
 
-    # Loop over all of the files in the directory
-    for file in os.listdir(path):
+    if not os.path.exists(outpath):
 
-        # Get the path for the specific file
-        file_path = os.path.join(path, file)
+        # List of images in the folder
+        images = []
 
-        # If the file is an image
-        if is_image(file_path):
+        # If no writer object is provided
+        if not writer:
+            # Create a new PdfWriter object
+            writer = PyPDF2.PdfWriter()
 
-            # Add the image to the image list
-            images.append(file_path)
+        # Loop over all of the files in the directory
+        for file in os.listdir(path):
 
-    # Loop over all of the images
-    for image in tqdm(range(len(images))):
+            # Get the path for the specific file
+            file_path = os.path.join(path, file)
 
-        # Open the image file
-        img = Image.open(images[image])
+            # If the file is an image
+            if is_image(file_path):
 
-        # Get the width and height of the image
-        # w,h = img.size
+                # Add the image to the image list
+                images.append(file_path)
 
-        img_name = images[image] + ".pdf"
+        # Loop over all of the images
+        for image in range(len(images)):
 
-        # Save the image in a temporary pdf at maximum resolution
-        img.save(img_name, "PDF", resolution = 100.0)
+            # Open the image file
+            img = Image.open(images[image])
 
-        wait = 10 # Time to wait (seconds)
-        counter = 0 # Time Counter (seconds)
+            # Get the width and height of the image
+            # w,h = img.size
 
-        # While the file has not been created
-        while not os.path.exists(img_name):
+            img_name = images[image] + ".pdf"
 
-            # Wait 1 second
-            time.sleep(1)
+            # Save the image in a temporary pdf at maximum resolution
+            img.save(img_name, "PDF", resolution=100.0)
 
-            # Add one to the counter
-            counter += 1
+            wait = 10  # Time to wait (seconds)
+            counter = 0  # Time Counter (seconds)
 
-            # If we have exceeded the wait limit
-            if counter > wait: 
+            # While the file has not been created
+            while not os.path.exists(img_name):
 
-                # Exit the loop
-                break
+                # Wait 1 second
+                time.sleep(1)
 
-        # Verify the file has been created 
-        if os.path.exists(img_name):
+                # Add one to the counter
+                counter += 1
 
-            # Create a pdf file reader for the temporary pdf
-            page = PyPDF2.PdfFileReader(images[image] + ".pdf")
+                # If we have exceeded the wait limit
+                if counter > wait:
 
-            # Add the page from the temporary pdf to the main pdf
-            writer.addPage(page.getPage(0))
-            
-            # Remove the temporary pdf
-            os.remove(images[image] + ".pdf")
+                    # Exit the loop
+                    break
 
-        else: # No file created
+            # Verify the file has been created
+            if os.path.exists(img_name):
 
-            print("File not opened: File '" + img_name + "' not created! Skipping ...")
+                # Create a pdf file reader for the temporary pdf
+                page = PyPDF2.PdfReader(images[image] + ".pdf")
 
-    # If there is at LEAST one page created in the main pdf
-    if writer.getNumPages() > 0:
-        
-        # Save the main pdf
-        writer.write(open(path + ".pdf", "wb"))
+                # Add the page from the temporary pdf to the main pdf
+                writer.add_page(page.pages[0])
 
-    else: # Filename not present
+                # Remove the temporary pdf
+                os.remove(images[image] + ".pdf")
 
-        print("Not processing folder:", path, "...")
+            else:  # No file created
 
-        
+                print(
+                    "File not opened: File '" + img_name + "' not created! Skipping ..."
+                )
+
+        # If there is at LEAST one page created in the main pdf
+        if len(writer.pages) > 0:
+
+            # Save the main pdf
+            writer.write(open(outpath, "wb"))
+
+        else:  # Filename not present
+
+            print("Not processing folder:", path, "...")
+
+    else:  # Skipped creating pdf
+        print(f"Skipped creating pdf '{outpath}' (already created)")
+
 
 # Check if the file is an image
 def is_image(filename):
 
     # Valid image file extensions
-    EXTENSIONS = [
-        '.png', '.jpg', '.jpeg', '.tiff'
-    ]
+    EXTENSIONS = [".png", ".jpg", ".jpeg", ".tiff"]
+
+    # Loop over the extensions
+    for extension in EXTENSIONS:
+
+        # If the filename contains the extension
+        if extension in filename:
+
+            # File is image, return true
+            return True
+
+    return False
+
+
+# Check if the file is an archive
+def is_archive(filename):
+
+    # Valid archive file extensions
+    EXTENSIONS = [".cbz", ".zip"]
 
     # Loop over all of the extensions
     for extension in EXTENSIONS:
@@ -118,17 +167,40 @@ def is_image(filename):
 
     return False
 
+
+def extract_archive(filename):
+
+    # Path to extract to, file extension
+    extract_path, _ = os.path.splitext(filename)
+    extract_folder = os.path.basename(extract_path)
+
+    # Get the path to the temp dir
+    temp_dir = tempfile.gettempdir()
+    target_path = os.path.join(temp_dir, extract_folder)
+
+    # Path does not exist
+    if not os.path.exists(target_path):
+
+        # Open the zip file
+        with zipfile.ZipFile(filename, "r") as zip:
+
+            # Extract archive to 'extract_to'
+            zip.extractall(target_path)
+
+    else:  # Path already exists
+        print(f"Skipped extracting archive '{filename}' (already extracted)")
+
+    return target_path
+
+
 # get_subfolders(path: String): [List, List]
-# Given a folder path, returns all of the sub 
+# Given a folder path, returns all of the sub
 # folders and image files in the directory and
 # returns them as a tuple.
-def get_subfolders(path):
+def get_subfolders(path, archive=False):
 
     # Path subpaths
     paths = []
-
-    # Path images
-    images = []
 
     # Loop over all of the files in the directory
     for file in os.listdir(path):
@@ -142,17 +214,16 @@ def get_subfolders(path):
             # Add the path to the paths list
             paths.append(file_path)
 
-        # If the file path is an image
-        elif is_image(file_path):
+        elif is_archive(file_path) and archive == True:
 
-            # Add the path to the images list
-            images.append(file_path)
+            # Extract the archive and add path to list
+            paths.append(extract_archive(file_path))
 
     # Loop over the paths
     for folder in paths:
 
         # Loop over all of the subfolders (again)
-        subpaths,subimages = get_subfolders(folder)
+        subpaths = get_subfolders(folder, archive)
 
         # Loop over all of the paths
         for p in subpaths:
@@ -160,52 +231,34 @@ def get_subfolders(path):
             # Add the paths to the list
             paths.append(p)
 
-        # Loop over all of the images
-        for i in subimages:
-
-            # Add the images to the list
-            images.append(i)
-
     # Return the paths list and images list as a tuple
-    return paths,images
+    return paths
+
 
 # Main Function
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    # List of paths
-    paths = []
+    # Parse the arguments
+    args = parser.parse_args()
 
-    # List of images
-    images = []
+    # Loop over the 'root' paths
+    for root_path in args.path:
 
-    # If more than one argument is provided
-    if len(sys.argv) > 1:
+        # List of paths
+        paths = [root_path]
 
-        # Loop over all of the ar guments
-        for directory in sys.argv[1:]:
+        # Use 'root_path' or output
+        output = args.output or root_path
 
-            # Add the directory to the list
-            paths.append(directory)
+        # Create the output folder, if required
+        os.makedirs(output, exist_ok=True)
+
+        # Loop over the paths
+        while len(paths) > 0:
+            directory = paths.pop()
 
             # Get all of the subfolders in the directory
-            p,i = get_subfolders(directory)
-
-            # Add all of the subfolders to the paths list
-            for x in p:
-                paths.append(x)
-
-            # Add all of the images to the images list
-            for x in i:
-                images.append(i)
-
-        print("Number of Directories: "+str(len(paths)))
-        print("Number of Images: "+str(len(images)))
-
-        # Loop over all of the paths, printing the progress bar
-        for folder in tqdm(range(len(paths))):
+            paths += get_subfolders(directory, args.archive)
 
             # Make the pdf for the folder
-            make_pdf(paths[folder])
-
-    else: # No arguments provided
-        print("Please drag the folder containing the images to convert onto this file!")
+            make_pdf(directory, output)
